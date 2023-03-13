c1ecc418c4fb929d060b7f1d749bb454ebd6cb61
Kubinic <Osiptsov84@gmail.com>
2023-03-13 13:13:07 +0700

    Homework_6

---
+++ task_41/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+Console.WriteLine("Введите число через пробел"); 
+string [] str = Console.ReadLine().Split(' ');
+
+Count_positive_numbers(str);
+
+void Count_positive_numbers(string [] str){
+int count = 0;
+
+    int[] arr = new int[str.Length];
+for (int i = 0; i < str.Length; i++)
+{ 
+    arr[i] = int.Parse(str[i]);
+   
+}
+     
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0){
+        count++;
+    }
+}
+Console.Write($"{count} чисел больше 0 ввёл пользователь.");
+}
---
+++ task_43/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача43 Напишите программу, которая найдёт точку 
+пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.WriteLine("Введите значения k1"); 
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значения b1"); 
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значения k2"); 
+double k2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значения b2"); 
+double b2 = double.Parse(Console.ReadLine()!);
+CrossingPoint(k1, b1, k2, b2);
+void CrossingPoint(double k1, double b1,double k2, double b2)
+{double x = (b2 - b1) / (-k2 + k1);
+double y = (k2 * x) + b2;
+if (x == y){
+    Console.Write($"точка пересечения двух прямыхy (X {x}; Y {x})");
+}
+else{
+    Console.Write("Прямые с заданым значением не пересикаются!");
+}
+}
